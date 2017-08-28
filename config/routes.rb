9d29439719549fb8d0c2ac40b98b01c843504ffd
EISUKE NAKAJIMA <ask8ask20ask@gmail.com>
2017-08-28 10:12:37 +0000

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: 'toppages#index'
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  root to: 'tasks#index'
-  
-  resources :tasks
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :tasks, only: [:create, :destroy, :new]
 end
